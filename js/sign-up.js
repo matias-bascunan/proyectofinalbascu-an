c66e888baf6a0b2b66baaa7d29e9389dd115bbeb
Matias Bascuñan <matias.bascunan@amcor.com>
2025-02-21 22:32:41 -0300

    arreglos3

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,16 +1,19 @@
-const signupform =document.querySelector ('#signupform') 
-signupform.addEvenListener 'submit', (e)=>{
-e.preventDefault() 
-const name= document.querySelector('#name')
-const email= document.querySelector('#email')
-const password= document.querySelector('#password')
+const signupForm = document.querySelector('#signupForm')
+signupForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const name = document.querySelector('#name').value
+    const email = document.querySelector('#email').value
+    const password = document.querySelector('#password').value
 
-const Users= JSON.parse(localStorage.getItem('users')) || []
-const isUserRegistered =Users.find (user =>user.email===email)
-if(isUserRegistered){
-    return alert('El usuario ya está registrado')
-}
-Users.push({name:name, email:email, password:password})
-localStorage.setItem('users', JSON.stringify())
+    const Users = JSON.parse(localStorage.getItem('users')) || []
+    const isUserRegistered = Users.find(user => user.email === email)
+    if(isUserRegistered){
+        return alert('El usuario ya esta registado!')
+    }
 
-}
+    Users.push({name: name, email: email, password: password})
+    localStorage.setItem('users', JSON.stringify(Users))
+    alert('Registro Exitoso!')
+    window.location.href = 'login.html'
+
+})
---
+++ main.js
@@ -0,0 +1,36 @@
+const btnCart = document.querySelector('.container-cart-icon')
+const containerCartProducts = document.querySelector('.container-cart-products')
+
+btnCart.addEventListener('click', () => {
+    containerCartProducts.classList.toggle('hidden-cart')
+})
+
+const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
+
+// Lista de todos los contenedores de productos
+const productsList = document.querySelector('.container-items');
+
+// Variable de arreglos de Productos
+let allProducts = [];
+
+const valorTotal = document.querySelector('.total-pagar');
+
+const countProducts = document.querySelector('#contador-productos');
+
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
+
+productsList.addEventListener('click', e => {
+	if (e.target.classList.contains('btn-add-cart')) {
+		const product = e.target.parentElement;
+
+		const infoProduct = {
+			quantity: 1,
+			title: product.querySelector('h2').textContent,
+			price: product.querySelector('p').textContent,
+		};
+
+		const exits = allProducts.some(
+			product => product.title === infoProduct.title
+		);
